faff7f7030f461a0f4b51eeb3a992f165f54496d
Adrienne <adrienneeaston@gmail.com>
2021-06-28 00:03:43 -0700

    minor adjustments

--- app.js
+++ app.js
@@ -1,33 +1,18 @@
-//proof of life
-
 const express = require('express');
 const iqResultsRoutes = require('./src/iq_results/routes');
 const app = express();
-// const path = require('path');
-
-// app.get('/', function(req,res) {
-//   res.sendFile(path.join(__dirname+'/public/index.html'));
-// });
 
 app.use(express.static('public'));
 
-// app.get('/', (req, res) => {
-//   res.sendFile('./public', { root: __dirname });
-// });
-
-// app.get('/contact', (req, res) => {
-//   res.sendFile('./landing-page/contact.html', { root: __dirname });
-// });
-
 app.use(express.json());
 
-app.get('/', (req, res) => {
-  res.send('This is home!');
-});
+// app.get('/', (req, res) => {
+//   res.send('This is home!');
+// });
 
-app.get('/post', (req, res) => {
-  res.send('This is post!');
-});
+// app.get('/post', (req, res) => {
+//   res.send('This is post!');
+// });
 
 app.use('/api/v1/iq_results', iqResultsRoutes);
 
