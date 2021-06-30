d8c7846cb0509029a00856a0a015a58d75e768ca
Adrienne <adrienneeaston@gmail.com>
2021-06-29 23:08:48 -0700

    ejs proof of life

--- app.js
+++ app.js
@@ -1,8 +1,26 @@
 const express = require('express');
+// const bodyParser = require('body-parser)');
+const cors = require('cors');
+const path = require('path');
+
 const iqResultsRoutes = require('./src/iq_results/routes');
+
 const app = express();
 
-app.use(express.static('public'));
+// app.use(bodyParser());
+app.use(cors());
+
+// app.use('views', path.join(__dirname, 'views'));
+// app.use(express.static('public'));
+app.set('view engine', 'ejs');
+
+app.get('/', (req, res) => {
+  res.render('index', { foo: 'bar' });
+});
+
+// app.get('/api/v1/iq_results/:id', (req, res) => {
+//   res.render('index', { firstname : iq_results(req.params.id) });
+// });
 
 app.use(express.json());
 
